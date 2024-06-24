8a4fe55c7e75c10065d071833d330184a21c0cef
Zhang Hua <zhanghua.00@qq.com>
2024-06-24 20:02:36 +0800

    Add dummy document to make formatter happy

--- E5Renewer.Modules.TomlParser.Tests/UnitTest1.cs
+++ E5Renewer.Modules.TomlParser.Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
 namespace E5Renewer.Modules.TomlParser.Tests;
 
+/// <summary>TODO</summary>
 [TestClass]
 public class UnitTest1
 {
+    /// <summary>TODO</summary>
     [TestMethod]
     public void TestMethod1()
     {
--- E5Renewer.Modules.YamlParser.Tests/UnitTest1.cs
+++ E5Renewer.Modules.YamlParser.Tests/UnitTest1.cs
@@ -1,8 +1,10 @@
 namespace E5Renewer.Modules.YamlParser.Tests;
 
+/// <summary>TODO</summary>
 [TestClass]
 public class UnitTest1
 {
+    /// <summary>TODO</summary>
     [TestMethod]
     public void TestMethod1()
     {
